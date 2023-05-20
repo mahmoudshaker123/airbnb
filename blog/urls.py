0257a50206439c812de5327dd38a096a7609dfda
mahmoud <mahmoud.shaker123123@gmail.com>
2023-05-20 17:33:33 +0300

    create blog template

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ app_name = 'blog'
 urlpatterns = [
     path('',PostList.as_view(),name='post_list'),
     path('<slug:slug>',PostDetail.as_view(), name='post_detail'),
+    path('category/<str:slug>',PostsByCategory.as_view(), name='post_by_category'),
+    path('tags/<slug:slug>',PostsByTags.as_view(), name='post_by_tags'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import *
+from taggit.models import Tag
+from django.db.models import Count
+from django.db.models import Q
+
 
 class PostList(ListView):
     model = Post
@@ -8,5 +14,33 @@ class PostList(ListView):
 
 class PostDetail(DetailView):
     model = Post
-
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = Category.objects.all().annotate(post_count = Count('post_category'))  
+        context["tags"] = Tag.objects.all()
+        context['recent_posts'] = Post.objects.all()[:3]
+        return context
+    
+
+
+class PostsByCategory(ListView):
+    model = Post    
+    def get_queryset(self):
+        slug = self.kwargs['slug']
+        # filter category by slug using Q class 
+        object_list = Post.objects.filter(
+            Q(category__name__icontains=slug)
+        )
+        return object_list
+    
+    
+class PostsByTags(ListView):
+    model = Post        
+    def get_queryset(self):
+        slug = self.kwargs['slug']
+        # filter tags by slug using Q class 
+        object_list = Post.objects.filter(
+            Q(tags__name__icontains=slug)
+        )
+        return object_list
