e6318fe609050e14dcc0634ce28aad37928fe750
mahmoud <mahmoud.shaker123123@gmail.com>
2023-05-25 13:50:34 +0300

    Update

--- about/views.py
+++ about/views.py
@@ -6,3 +6,9 @@ from .models import *
 
 class AboutView(ListView):
     model = FAQ
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["about"] = About.objects.last()
+        return context
+    
